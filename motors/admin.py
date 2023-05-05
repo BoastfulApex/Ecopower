c28ae2bd3c9d626b471967a2c1a21333a1cc3751
BoastfulApex <jahongirnormuminov8@gmail.com>
2023-05-05 17:23:34 +0500

    admin.setting added

--- motors/admin.py
+++ motors/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+admin.site.register(About)
+admin.site.register(Brand)
+admin.site.register(Car)
+admin.site.register(Video)
+admin.site.register(Image)
+admin.site.register(FAQ)
