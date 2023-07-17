c5874173e5b2fa1e1ccfc3cd65155aa830b6e226
BoastfulApex <jahongirnormuminov8@gmail.com>
2023-07-18 00:24:47 +0500

    card edited

--- motors/models.py
+++ motors/models.py
@@ -52,6 +52,39 @@ class Car(models.Model):
     run = models.FloatField(default=0)
 
     top = models.BooleanField()
+
+    elektr_dvigateli_uz = models.CharField(max_length=10000, null=True, blank=True)
+    elektr_dvigateli_en = models.CharField(max_length=10000, null=True, blank=True)
+    elektr_dvigateli_ru = models.CharField(max_length=10000, null=True, blank=True)
+    batareya_quvvati_uz = models.CharField(max_length=10000, null=True, blank=True)
+    batareya_quvvati_en = models.CharField(max_length=10000, null=True, blank=True)
+    batareya_quvvati_ru = models.CharField(max_length=10000, null=True, blank=True)
+    taxminiy_masofa_uz = models.CharField(max_length=10000, null=True, blank=True)
+    taxminiy_masofa_en = models.CharField(max_length=10000, null=True, blank=True)
+    taxminiy_masofa_ru = models.CharField(max_length=10000, null=True, blank=True)
+    drayv_uz = models.CharField(max_length=10000, null=True, blank=True)
+    drayv_en = models.CharField(max_length=10000, null=True, blank=True)
+    drayv_ru = models.CharField(max_length=10000, null=True, blank=True)
+    tezlashuv_uz = models.CharField(max_length=10000, null=True, blank=True)
+    tezlashuv_en = models.CharField(max_length=10000, null=True, blank=True)
+    tezlashuv_uz = models.CharField(max_length=10000, null=True, blank=True)
+    zaryadlash_vaqti_en = models.CharField(max_length=10000, null=True, blank=True)
+    zaryadlash_vaqti_uz = models.CharField(max_length=10000, null=True, blank=True)
+    zaryadlash_vaqti_ru = models.CharField(max_length=10000, null=True, blank=True)
+    zaryadlash_porti_uz = models.CharField(max_length=10000, null=True, blank=True)
+    zaryadlash_porti_en = models.CharField(max_length=10000, null=True, blank=True)
+    zaryadlash_porti_ru = models.CharField(max_length=10000, null=True, blank=True)
+    olchamlari_uz = models.CharField(max_length=10000, null=True, blank=True)
+    olchamlari_en = models.CharField(max_length=10000, null=True, blank=True)
+    olchamlari_ru = models.CharField(max_length=10000, null=True, blank=True)
+    otirish_hajmi_uz = models.CharField(max_length=10000, null=True, blank=True)
+    otirish_hajmi_ru = models.CharField(max_length=10000, null=True, blank=True)
+    otirish_hajmi_en = models.CharField(max_length=10000, null=True, blank=True)
+    yuk_tashish_hajmi_uz = models.CharField(max_length=10000, null=True, blank=True)
+    yuk_tashish_hajmi_en = models.CharField(max_length=10000, null=True, blank=True)
+    yuk_tashish_hajmi_ru = models.CharField(max_length=10000, null=True, blank=True)
+
+
     
     def __str__(self):
         return self.name_uz
